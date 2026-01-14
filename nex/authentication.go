c69aa595c23fb9a54509c0e9df4e07851d334227
TraceEntertains <atw3710@gmail.com>
2026-01-14 15:07:09 -0500

    Initial commit

---
+++ nex/authentication.go
@@ -0,0 +1,54 @@
+package nex
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/PretendoNetwork/monster-hunter-4/globals"
+	"github.com/PretendoNetwork/nex-go/v2"
+)
+
+func StartAuthenticationServer() {
+	globals.AuthenticationServer = nex.NewPRUDPServer()
+
+	globals.AuthenticationEndpoint = nex.NewPRUDPEndPoint(1)
+	globals.AuthenticationEndpoint.ServerAccount = globals.AuthenticationServerAccount
+	globals.AuthenticationEndpoint.AccountDetailsByPID = globals.AccountDetailsByPID
+	globals.AuthenticationEndpoint.AccountDetailsByUsername = globals.AccountDetailsByUsername
+
+	globals.AuthenticationServer.BindPRUDPEndPoint(globals.AuthenticationEndpoint)
+
+	globals.AuthenticationServer.LibraryVersions.SetDefault(nex.NewLibraryVersion(2, 7, 3))
+	globals.AuthenticationServer.AccessKey = "625bf7f9"
+
+	globals.AuthenticationEndpoint.OnData(func(packet nex.PacketInterface) {
+		request := packet.RMCMessage()
+		protocol := globals.GetProtocolByID(request.ProtocolID)
+
+		//userData, err := globals.UserDataFromPID(packet.Sender().PID())
+
+		// var username string
+		// if err != 0 {
+		// 	username = "Failed to get username"
+		// } else {
+		// 	username = userData.Username
+		// }
+
+		fmt.Println("== Monster Hunter 4 - Auth ==")
+		//fmt.Printf("User: %d\n", packet.Sender().PID())
+		fmt.Printf("Protocol: %d (%s)\n", request.ProtocolID, protocol.Protocol())
+		fmt.Printf("Method: %d (%s)\n", request.MethodID, protocol.GetMethodByID(request.MethodID))
+		fmt.Println("===============")
+	})
+
+	globals.AuthenticationEndpoint.OnError(func(err *nex.Error) {
+		globals.Logger.Errorf("Auth: %v", err)
+	})
+
+	registerCommonAuthenticationServerProtocols()
+
+	port, _ := strconv.Atoi(os.Getenv("PN_MH4_AUTHENTICATION_SERVER_PORT"))
+
+	globals.AuthenticationServer.Listen(port)
+}
